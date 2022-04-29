d70caebc3df862c6e7f4f9497b7042eb99b5f3e1
Bhavya <103767731+Bhavya1130@users.noreply.github.com>
2022-04-29 18:29:24 +0530

    21 Matchsticks

---
+++ Bhavya/21 Matchsticks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int arr[] = { 1, 3, 5, 3 };
+ 
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i += 1) {
+        cout << 5 - arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
