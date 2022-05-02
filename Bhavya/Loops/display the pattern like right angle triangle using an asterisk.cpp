e26acadd68a1006889295ddb7a8cc3756a33e42a
Bhavya <103767731+Bhavya1130@users.noreply.github.com>
2022-05-02 20:00:18 +0530

    display right angle triangle using an asterisk

---
+++ Bhavya/Loops/display the pattern like right angle triangle using an asterisk.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    for(int i {} ; i <= n; i++){
+        for(int j {} ; j < i; j++){
+            cout << "*";
+        }
+        cout << endl; 
+    }
+    return 0;
+}
